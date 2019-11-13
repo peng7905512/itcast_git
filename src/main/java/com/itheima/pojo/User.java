7cb983465e2a34d582f23014df42c34eb4aaf5b4
PJadmin <690414258@qq.com>
2019-11-13 20:33:25 +0800

    又修改啦

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -2,13 +2,5 @@ package com.itheima.pojo;
 
 public class User {
     private String name;
-    private String email;
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public User() {
-    }
 }
